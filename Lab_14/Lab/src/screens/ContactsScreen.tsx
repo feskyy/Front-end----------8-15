f6dca3c0c26b3a79854397f496538a48126af864
feskyy <fesky@icloud.com>
2026-04-18 12:27:12 +0500

    lab_14

--- Lab_14/Lab/src/screens/ContactsScreen.tsx
+++ Lab_14/Lab/src/screens/ContactsScreen.tsx
@@ -13,8 +13,32 @@ export default function ContactsScreen() {
 
     const renderItem = useCallback(({ item }: { item: Contact }) => {
         return (
-        )
+            <TouchableOpacity 
+                style = {styles.contsctItem}
+                onPress = {() => Alert.alert('Selected', item.email)}
+                activeOpacity = {0.7}
+            >
+                <View style = {[styles.avatar, { backgroundColor: item.avatarColor }]}>
+                    <Text style = {styles.avatarText}>{item.firstName[0]}{item.lastName[0]}</Text>
+                </View>
+
+                <View style = {styles.contactDetails}>
+                    <Text style = {styles.contactName}>{item.firstName} {item.lastName}</Text>
+                    <Text style = {styles.contactEmail}>{item.email}</Text>
+                </View>
+            </TouchableOpacity>
+        );
     }, []);
-}
+
+
+    const keyExtractor = useCallback((item: Contact) => item.id, []);
+
+
+    const getItemLayout = useCallback((data: any, index: number) => ({
+        length: IETM_HEIGHT,
+        offset: IETM_HEIGHT * index,
+        index,
+    }), []);
+};
 
 
